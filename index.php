9e3f9ac4b529865cbdb6a3b2ff8b1fee7a65d38d
Tony-Milukov <89128409+Tony-Milukov@users.noreply.github.com>
2022-12-14 18:42:03 +0100

    HW2, я если честно незнаю правильно ли сделал 2.3, если я правильно понял дз, то нало считать до 5 с заданого числа, и потом обратно проверяя при этом это число...

--- index.php
+++ index.php
@@ -1,32 +1,81 @@
 <?php
-    //Homework 1.1
+    //Homework 2.1
+//    $answer = null;
+//    $question = "В каком году произошло крещение Руси? ";
+//    $givedAnsewers = "810, 990 или 740 год";
+//    $answers = [740,810];
+//    $rightAnswer = 990;
+//    do {
+//            $answer = (int)readline("$question\n$givedAnsewers");
+//
+//        if(in_array($answer,$answers)) {
+//            print("Извините, ваш ответ не верный");
+//            break;
+//        }
+//    } while($answer !== $rightAnswer);
+//    echo   $answer == $rightAnswer ? "Поздравляю, вы правильно ответили на этот вопрос!" : null;
+//
 
-//    $name = readline("whats your name ");
-//    $age = readline("whats your age ");
-//    echo("your name is $name and $age years old ");
+    //Homework 2.2
+//    $userName = (string) readline("whats your name? ");
+//    $tasks = [];
+//    $howManyTasks = null;
+   function isInteger ($input) {
+       return ctype_digit(strval($input));
+   }
+//    do {
+//        $howManyTasks = readline("How many tasks have you for today? ");
+//    } while (!isInteger($howManyTasks));
+//
+//    if($howManyTasks >= 1) {
+//       for($i = 0; $i != $howManyTasks; $i++) {
+//           $nameOfTask =  (string) readline("What have you to do? ");
+//           do {
+//               $taskTime = readline("How long will it take? ");
+//           } while (!isInteger($taskTime));
+//           $newTask = [
+//               "nameOfTask" => $nameOfTask,
+//               "taskTime" =>  $taskTime
+//           ];
+//           $tasks[] = $newTask;
+//           $newTask = [];
+//       }
+//        echo ("Your name is $userName \n\n" );
+//        echo ("You have $howManyTasks ". count($tasks) >= 2 ? "tasks" : "task" . "for today \n\n");
+//        $allTime = 0;
+//        foreach($tasks as $index => $item) {
+//            echo($index+1 . ".  " . $item["nameOfTask"] . "(". $item["taskTime"] . "h)\n");
+//            $allTime += $item["taskTime"];
+//        }
+//        echo ("\nThat will take you $allTime"."h");
+//
+//    } else {
+//        echo "You don't seem to have tasks for the day! enjoy it";
+//    }
 
-    //Homework 1.2
-    $userName = readline("whats your name? ");
-    $tasks = [];
-    $howManyTasks = readline("How many tasks have you for today? ");
-    if($howManyTasks >= 1) {
-       for($i = 0; $i != $howManyTasks; $i++) {
-           $nameOfTask =  readline("What have you to do? ");
-           $taskTime = readline("How long will it take? ");
-           $newTask = [
-               "nameOfTask" => $nameOfTask,
-               "taskTime" =>  $taskTime
-           ];
-           $tasks[] = $newTask;
-           $newTask = [];
-       }
-        echo ("Your name is $userName \n\n" );
-        echo ("You have " . count($tasks) . " task for today \n\n");
-        $allTime = 0;
-        foreach($tasks as $index => $item) {
-            echo($index+1 . ".  " . $item["nameOfTask"] . "(". $item["taskTime"] . "h)\n");
-            $allTime += $item["taskTime"];
+// Homework 2.3
+    do {
+        $fingerNumber = readline("input finger number 1-5 ");
+        if(isInteger($fingerNumber)) {
+            if ($fingerNumber >= 1 && $fingerNumber <= 5) {
+                break;
+            }
         }
-        echo ("That will take you $allTime"."h");
-
-    }
+    } while (true);
+    $fingers = "12345";
+        if ($fingerNumber >= 1 || $fingerNumber < 5) {
+           if($fingerNumber !== 5) {
+               while ($fingerNumber-1 !== 4) {
+                   $fingerNumber++;
+                   echo $fingerNumber;
+               }
+           }
+    } if ($fingerNumber == 5) {
+            while ($fingerNumber-1 !== 0) {
+                $fingerNumber--;
+                echo $fingerNumber;
+                if($fingerNumber-1 == 0) {
+                    break;
+                }
+            }
+        }
